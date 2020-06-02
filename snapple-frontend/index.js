1cfe05f1fb240db6c27bc0d6ccb05cfcdc5a286f
dlyons8 <dlyons8@binghamton.edu>
2020-06-02 11:10:11 +0300

    add consumable class

--- snapple-frontend/index.js
+++ snapple-frontend/index.js
@@ -120,3 +120,28 @@ class Snake {
         }
     }
 }
+
+class Consumable {
+    constructor(imageFile){
+        this.image = this.loadImage(imageFile)
+        this.x = Math.floor(Math.random() * 20)
+        this.y = Math.floor(Math.random() * 20)
+    }
+
+    loadImage(imageFile) {
+        const image = new Image();
+        image.onload = function() {}
+        image.src = imageFile;
+    
+        return image;
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.x * gridSize, this.y * gridSize, gridSize, gridSize)
+    }
+
+    spawn() {
+      this.x = Math.floor(Math.random() * 20)
+      this.y = Math.floor(Math.random() * 20)
+    }
+}
