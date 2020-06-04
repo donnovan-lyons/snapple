41007622b1530c65fd3e8c20433d6d718d6ce6b8
dlyons8 <dlyons8@binghamton.edu>
2020-06-04 11:42:20 +0300

    implement static function to add high scores at end

--- snapple-frontend/src/javascripts/game.js
+++ snapple-frontend/src/javascripts/game.js
@@ -5,6 +5,7 @@ class Game {
         this.score = score
         this.apple = apple
         this.skull = skull
+        this.completed = false
     }
 
     checkHighScore() {
@@ -45,13 +46,49 @@ class Game {
     start() {
         document.addEventListener("keydown", keyDownEvent); 
         let x = 8;
-        let interval = setInterval(this.draw.bind(this), 1000 / x);
+        interval = setInterval(this.draw.bind(this), 1000 / x);
     }
 
     gameOver() {
         console.log("Game Over")
+        clearInterval(interval);
+        fetch("http://localhost:3000/games/high_scores")
+        .then(resp => resp.json())
+        .then(json => Game.appendHighScore(json))
         canvas.style.display = "none"
         highScoreScreen.style.display = ""
     }
+
+    static appendHighScores(data) {
+        data.forEach(element => {
+            let tr = document.createElement('tr');
+            let name = document.createElement('td')
+            let score = document.createElement('td')
+            name.innerText = element.user.name
+            score.innerText = element.score
+            tr.appendChild(name)
+            tr.appendChild(score)
+            document.getElementById('high_scores').appendChild(tr)
+        });
+    }
+
+    s
+
+    pause() {
+        clearInterval(interval);
+    }
+
+    save() {
+
+    }
     
 }
+
+// fetch("http://localhost:3000/games", {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json"},
+//         body: JSON.stringify({trainer_id: trainerId})
+//     })
+//     .then(resp => resp.json())
+//     .then(json => appendPokemon(json));
