1ea07bb3a46907c7186b65d01b108c99dfb0c857
dlyons8 <dlyons8@binghamton.edu>
2020-06-09 14:46:39 +0300

    fixed restore bug

--- snapple-backend/config/routes.rb
+++ snapple-backend/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :games, only: [:show, :create, :update]
   get 'games/high_scores', to: 'games#high_scores'
+  resources :games, only: [:show, :create, :update]
   get 'games/restore/:name', to: 'games#restore'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
