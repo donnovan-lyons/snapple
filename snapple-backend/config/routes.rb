acefa084a2d023cf50899f855a6d7ebba320091a
dlyons8 <dlyons8@binghamton.edu>
2020-06-10 11:34:23 +0300

    made controller api version 1 specific

--- snapple-backend/config/routes.rb
+++ snapple-backend/config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  get 'games/high_scores', to: 'games#high_scores'
-  resources :games, only: [:show, :create, :update]
-  get 'games/restore/:name', to: 'games#restore'
+  # get 'games/high_scores', to: 'games#high_scores'
+  # resources :games, only: [:show, :create, :update]
+  # get 'games/restore/:name', to: 'games#restore'
+  namespace :api do
+    namespace :v1 do
+      resources :games do
+        collection do
+          get 'high_scores', to: 'games#high_scores'
+          get 'restore/:name', to: 'games#restore'
+        end
+      end
+      resources :games, only: [:show, :create, :update]
+    end
+  end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
