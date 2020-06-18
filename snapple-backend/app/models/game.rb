30882c1dde15185ca62ec955f40a9607a2ed0c7a
dlyons8 <dlyons8@binghamton.edu>
2020-06-18 21:18:28 +0300

    comment edit

--- snapple-backend/app/models/game.rb
+++ snapple-backend/app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ApplicationRecord
     belongs_to :user, optional: true
     has_one :snake
     scope :top_ten, -> { where(completed: true).order('score DESC').limit(10) }
-    scope :user_restore, -> (id) do #where user id present and latest of that User's completed false games
+    scope :user_restore, -> (id) do #where user id present and latest of that User's incompleted games
         where("(user_id = ?) AND (completed = ?)", id, false).order(created_at: :desc).limit(1)
     end
 end
